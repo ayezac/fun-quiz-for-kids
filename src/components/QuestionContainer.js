2035bc9bc9f8d21e3db39d15254e4b291c3fff32
ayezac <ayeshazac@gmail.com>
2019-10-15 11:25:15 +0200

    Removed check answer feature, removed button and input outline, removed autocomplete

--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './styles/questionContainer.css';
-import CheckAnswer from './CheckAnswer';
 import Results from './Results';
 
 const QuestionContainer = (props) => {
@@ -8,15 +7,11 @@ const QuestionContainer = (props) => {
     const [question, setQuestion] = useState(props.quizData[index]);
     const [givenAnswer, setGivenAnswer] = useState('');
     const [result, setResult] = useState(0);
-    const [correct, setCorrect] = useState(false);
-    const [wrong, setWrong] = useState(false);
     const [getResults, setGetResults] = useState(false);
 
     const quizLength = props.quizData.length;
 
     const handleClick = () => {
-        setCorrect(false);
-        setWrong(false);
         //first check answer and set result
         givenAnswer.toLocaleLowerCase()===question.correctAnswer && setResult(result + 1)
         //then reset input area
@@ -31,22 +26,29 @@ const QuestionContainer = (props) => {
             setNextIndex(index+1);
         }
     }
+    //in the case the enter key is used to go to the next question
+    const handleKeyDown= (e) => {
+        if(e.keyCode === 13){
+          //first check answer and set result
+          givenAnswer.toLocaleLowerCase()===question.correctAnswer && setResult(result + 1)
+          //then reset input area
+          setGivenAnswer('');
+          //then go to next question if it exists
+          if(index<(props.quizData.length-1)) {
+              setNextIndex(index+1); 
+              setQuestion(props.quizData[index + 1]);
+          }
+          //once last question is reached, show results
+          else if (index===(props.quizData.length-1)){
+              setNextIndex(index+1);
+          }
+        }
+     }
 
     const handleChange = e => {
-        setCorrect(false);
-        setWrong(false);
         setGivenAnswer(e.target.value)
     }
 
-    const checkAnswer=()=>{
-        givenAnswer.toLocaleLowerCase()===question.correctAnswer && setCorrect(true);
-        givenAnswer.toLocaleLowerCase()!==question.correctAnswer && setWrong(true);
-    }
-
-    const handleCheck =()=>{
-checkAnswer();
-       
-    }
     const handleGetResults=()=>{
         setGetResults(true);
     }
@@ -54,7 +56,6 @@ checkAnswer();
 
     return(
         <div>
-            <CheckAnswer  correct={correct} wrong={wrong}/>
             {!getResults && (
                 <div>
                     <h2 className="question">{props.categoryQuestion}</h2>
@@ -63,10 +64,9 @@ checkAnswer();
                     <div id='question-container'> 
                         {props.picture ===false && <h3 className="questions">{question.question}</h3>}
                         {props.picture && <img src = {question.question} alt='flags' id="flag"/>}
-                        <input type="text" onChange={handleChange} value ={givenAnswer} id="answer"/>
+                        <input type="text" onChange={handleChange} value ={givenAnswer} id="answer" autoComplete='off' onKeyDown={handleKeyDown}/>
                         <div className='button-container'>
                             <button onClick={handleClick} className='button'>Next</button> 
-                            <button onClick={handleCheck} className='button'>Check</button> 
                         </div>
                     </div>  
                     )} 
